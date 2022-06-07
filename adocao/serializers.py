18fad7b9f250e65c3c0c35093e2de78b6205880b
aluiziocatao <aluizio2210@gmail.com>
2022-06-07 08:37:52 -0500

    Classroom 04

--- adocao/serializers.py
+++ adocao/serializers.py
@@ -5,6 +5,7 @@ from .models import Adocao
 from pet.serializers import PetSerializer
 from pet.models import Pet
 
+
 class AdocaoSerializer(serializers.ModelSerializer):
     pet = PetSerializer(many=False, read_only=True)
     pet_id = serializers.PrimaryKeyRelatedField(
@@ -13,12 +14,12 @@ class AdocaoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Adocao
-        fields = ('id', 'email', 'valor', 'pet', 'pet_id')
+        fields = ("id", "email", "valor", "pet", "pet_id")
 
     def create(self, validated_data):
-        validated_data['pet'] = validated_data.pop('pet_id')
+        validated_data["pet"] = validated_data.pop("pet_id")
         return super().create(validated_data)
-        
+
     def validate_valor(self, value):
         if value < 10:
             raise serializers.ValidationError("Deve ser maior que 10")
